00bf76c1593308ee3d99aece304ba074259bec2f
spectre-ksa <49376807+spectre-ksa@users.noreply.github.com>
2019-07-09 21:50:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -527,7 +527,7 @@ client.on('guildMemberAdd', member => {
     .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
     .setColor('RANDOM')
     .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
-var channel =member.guild.channels.find('name', 'chat-arabjoker')
+var channel =member.guild.channels.find('name', 'welcom')
 if (!channel) return;
 channel.send({embed : embed});
 });
